1b7a10838415dcd2abf0dd890f9ad643035b8c95
iiitsanni <annikamaringer@online.de>
2026-03-11 12:18:26 -0400

    added the logo and redid the Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
-import styled from 'styled-components';
-import PurpleButton from "../components/PurpleButton.jsx";
 
-const Form = () => {
+const Login = () => {
     return (
-    );}
+        <div>
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <legend className="fieldset-legend">Login</legend>
+
+                <label className="label">Email or Phone number</label>
+                <input type="email" className="input" placeholder="Email" />
+
+                <label className="label">Password</label>
+                <input type="password" className="input" placeholder="Password" />
+
+                <div className='flex f'>
+                <button className="btn btn-neutral mt-4">Login</button>
+                <button className="btn btn-neutral mt-4">Register</button>
+                </div>
+            </fieldset>
+        </div>
+    );
+};
+
+export default Login;
