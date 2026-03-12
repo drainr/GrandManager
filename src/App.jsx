6a18c255af2b7a4b328ae92ce6b8c68f8b44e4c2
iiitsanni <annikamaringer@online.de>
2026-03-12 14:22:23 -0400

    finished up the login and signup design

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Dashboard from './pages/Dashboard.jsx';
  import Login from './pages/Login.jsx';
+import SignUp from "./pages/SignUp.jsx";
 //import Calender from './pages/Calender.jsx';
 
 function App() {
@@ -9,8 +10,8 @@ function App() {
 
   return (
     <>
-       <Login />
-    </>
+       <Login/>
+       </>
   )
 }
 
