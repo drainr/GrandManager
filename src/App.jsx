5e68440336f76111165a4ecd7629bdd514a6415e
ibowers27 <i.bowers27@ncf.edu>
2026-03-13 15:00:39 -0400

    moving buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import './App.css'
-//import Dashboard from './pages/Dashboard.jsx';
-//import Login from './pages/Login.jsx';
+import Login from './pages/Login.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 import SignUp from "./pages/SignUp.jsx";
-import Calendar from "daisyui/components/calendar/index.js";
 import Calender from "./pages/Calender.jsx";
-import Login from "./pages/Login.jsx";
-//import Calender from './pages/Calender.jsx';
+import InspirationalPopup from "./pages/InspoPopUp.jsx";
 
 function App() {
 
 
   return (
-       <div className="m-10">
-           <Calender />
-           <div/><div className="m-10">
-           <Login /></div>
-           <div className="m-10">
-           <SignUp /></div>
-        </div>
-
+    <Calender/>
   )
 }
 
