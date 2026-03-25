812330acd11e45a4bae2814969c0aade0ab67aee
iiitsanni <annikamaringer@online.de>
2026-03-25 14:53:42 -0400

    active day being highlighted

--- src/components/list/WeeklyBarConfig.jsx
+++ src/components/list/WeeklyBarConfig.jsx
@@ -3,11 +3,11 @@ import YellowButton from "../YellowButton.jsx";
 import PurpleButton from "../PurpleButton.jsx";
 
 // weekly bar at top
-const WeeklyBar = ({ weekDays, onDayClick }) => {
+const WeeklyBar = ({ weekDays,activeDay,todayIndex, onDayClick }) => {
   return (
         <div className="grid grid-cols-7 gap-2">
-            {weekDays.map((day, activeDay) => {
-                const isSelected = activeDay === day;
+            {weekDays.map((day, index) => {
+                const isSelected =activeDay === day || (activeDay === null && index === todayIndex);
                 const ButtonComponent = isSelected ? YellowButton : PurpleButton;
                 return (
                 <div key={day} className="flex justify-center">
