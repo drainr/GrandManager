e6c0c1a59b35df6c936d9dae715b8f8f31948948
amaringer-ncf <amaringer@ncf.edu>
2026-03-25 20:04:29 -0400

    experimenting with the colors

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-            <div className="fixed top-1 left-[500px] right-[500px] rounded-2xl p-2 bg-[#EBB537] shadow-xl">
-                <h1 className="text-sm shrikhand-regular text-[#6E3FA3]">GrandManager</h1>
+            <div className="fixed top-2 left-0 w-full p-2 bg-[#4d2c72] shadow-xl">
+                <h1 className="text-sm shrikhand-regular text-[#EBB537]">GrandManager</h1>
 
         </div>
     );
