177c877c885c57ebed03dfd525a98cbd78ac04e6
amaringer-ncf <amaringer@ncf.edu>
2026-03-26 22:13:16 -0400

    ReadMe copied from old project and navbar has a dropdown and profile icon

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import RedButton from '../components/RedButton';
 import { logout } from '../firebase/auth';
+import { CgProfile } from "react-icons/cg";
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -14,7 +15,23 @@ const Navbar = () => {
     return (
         <div className="fixed top-0 left-0 w-full p-2 bg-[#4d2c72] shadow-xl flex items-center justify-between z-50">
             <h1 className="text-sm shrikhand-regular text-[#EBB537]">GrandManager</h1>
-            <RedButton text="Log Out" onClick={handleLogout} />
+            <div className="dropdown dropdown-end mr-4">
+
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar overflow-hidden">
+                    <div className="w-full h-full rounded-full flex items-center justify-center bg-[#1B2851]">
+                        <CgProfile className="w-full h-full text-[#EBB537]"/>
+                    </div>
+                </div>
+
+                <ul
+                    tabIndex={0}
+                    className="flex justify-center menu menu-sm dropdown-content bg-[#EBB537] text-white rounded-box z-[1] mt-3 p-2 shadow-2xl border border-[#4d2c72]"
+                >
+                    <li >
+                        <RedButton text='Logout' onClick={handleLogout}/>
+                    </li>
+                </ul>
+        </div>
         </div>
     );
 };
