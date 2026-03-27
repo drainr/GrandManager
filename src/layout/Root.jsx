10b360f2dc0a4acdc4c844e585e3db8e11c581b7
ibowers27 <i.bowers27@ncf.edu>
2026-03-26 21:52:22 -0400

    Fixed Pop-up timing to 5 mins & welcome

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Outlet } from "react-router";
 import Footer from "../pages/Footer";
 import Navbar from "../pages/Navbar";
+import InspirationalPopup from "../pages/InspoPopUp";
 
 const Root = () => {
   return (
     <>
         <Navbar />
         <Outlet />
+        <InspirationalPopup />
     </>
   );
 };
